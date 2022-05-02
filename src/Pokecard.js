67e2de600ad1dc2eff83bf363f5a910555e8f955
nwebb123 <68877910+nwebb123@users.noreply.github.com>
2022-05-02 19:47:16 -0400

    update

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -2,18 +2,23 @@ import React, {Component} from 'react';
 import './Pokecard.css';
 
 //this variable can be used further down to shorten the amount of markup required to make calls to API
+// const POKE_API =
+// 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+
 const POKE_API =
-'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+    'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/';
+
+let padToThree = (pokeID) => (pokeID <= 151 ? `00${pokeID}`.slice(-3): pokeID)
 
 class Pokecard extends Component {
     render (){
-        let imgSrc = `${POKE_API}${this.props.id}.png`;
+        let imgSrc = `${POKE_API}${padToThree(this.props.id)}.png`;
         return (
             <div className="Pokecard">
-                <h1>{this.props.name}</h1>
-                <img src={imgSrc} alt={this.props.name} />
-                <div>Type: {this.props.type}</div>
-                <div>EXP: {this.props.exp}</div>
+                <h1 className="Pokecard-name">{this.props.name}</h1>
+                <img className="Pokecard-img" src={imgSrc} alt={this.props.name} />
+                <div className="Pokecard-data">Type: {this.props.type}</div>
+                <div className="Pokecard-data">EXP: {this.props.exp}</div>
             </div>
         )
     }
--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,26 +1,40 @@
 import React, { Component } from 'react';
 import Pokecard from './Pokecard';
+import './Pokedex.css';
 
 class Pokedex extends Component{
     static defaultProps = {
         pokemon : [
+            
+            { id: 1, name:'Bulbasuar', type: 'Grass/Poison', base_experience: 62 },
+            { id: 2, name:'Ivysaur', type: 'Grass/Poison', base_experience: 62 },
+            { id: 3, name:'Venusaur', type: 'Grass/Poison', base_experience: 62 },
             { id: 4, name:'Charmander', type: 'Fire', base_experience: 62 },
-            { id: 7, name:'Squirtle', type: 'Water', base_experience: 63 },
-            { id: 11, name:'Metapod', type: 'Bug', base_experience: 72 },
-            { id: 12, name:'Butterfree', type: 'Flying', base_experience: 178 },
-            { id: 25, name:'Pikachu', type: 'Electric', base_experience: 112 },
-            { id: 39, name:'Jigglypuff', type: 'Normal', base_experience: 95 },
-            { id: 94, name:'Gengar', type: 'Poison', base_experience: 225 },
-            { id: 133, name:'Eevee', type: 'Normal', base_experience: 65 },
+            { id: 5, name:'Charmeleon', type: 'Fire', base_experience: 62 },
+            { id: 6, name:'Charizard', type: 'Fire/Flying', base_experience: 62 },
+            { id: 7, name:'Squirtle', type: 'Water', base_experience: 62 },
+            { id: 8, name:'Wartortle', type: 'Water', base_experience: 63 },
+            { id: 9, name:'Blastoise', type: 'Water', base_experience: 63 },
+            // { id: 10, name:'Squirtle', type: 'Water', base_experience: 63 },
+            // { id: 11, name:'Squirtle', type: 'Water', base_experience: 63 },
+            // { id: 12, name:'Metapod', type: 'Bug', base_experience: 72 },
+            // { id: 13, name:'Butterfree', type: 'Flying', base_experience: 178 },
+            // { id: 25, name:'Pikachu', type: 'Electric', base_experience: 112 },
+            // { id: 39, name:'Jigglypuff', type: 'Normal', base_experience: 95 },
+            // { id: 94, name:'Gengar', type: 'Poison', base_experience: 225 },
+            // { id: 133, name:'Eevee', type: 'Normal', base_experience: 65 },
         ]
     }
     render(){
         return(
             <div className="Pokedex">
-                <h1>Pokedex here</h1>
-                {this.props.pokemon.map((p)=>(
+                <h1 className="Pokedex-title">Pokemon Original Starters</h1>
+                <div className="Pokedex-cards">
+                    {this.props.pokemon.map((p)=>(
                     <Pokecard id={p.id} name={p.name} type={p.type} exp={p.base_experience} />
-                ))}
+                ))} 
+                </div>
+               
             </div>
         )
     }
